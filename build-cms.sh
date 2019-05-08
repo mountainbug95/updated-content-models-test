b67739d825f9d6e7355c2a19ee98a76b06abb68d
mountainbug95 <smithtn2@goldmail.etsu.edu>
2019-05-08 13:47:31 -0400

    Hyperspace test 3

--- build-cms.sh
+++
@@ -1,1 +0,0 @@
-npm install --no-save --no-package-lock contentful@^7.2.0 fs-extra@^6.0.1 gray-matter@^4.0.2 js-yaml@^3.12.0 lodash@^4.17.10 && node ./build-cms.js
--- build.sh
+++
@@ -1,2 +0,0 @@
-./build-cms.sh
-NF_IMAGE_VERSION=1 bash ./netlify-build-fix.sh && LD_LIBRARY_PATH=$(pwd)/tmp/usr/lib/x86_64-linux-gnu $(pwd)/tmp/usr/local/bin/hugo
